d05d972e374484d6081a5accfa253ae37ca6c6f3
kavindunaveen <it21226878@my.sliit.lk>
2024-03-18 12:39:25 +0530

    If user alreeady logged in,then redirect to  home screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -3,80 +3,94 @@ import { View, Text, StyleSheet, TextInput, ActivityIndicator, TouchableOpacity,
 import { useNavigation } from '@react-navigation/native';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const auth = FIREBASE_AUTH;
-    const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        // Set Firebase Auth persistence with AsyncStorage
-        const initializeFirebaseAuth = async () => {
-            try {
-                const persistence = getReactNativePersistence(AsyncStorage);
-                await setPersistence(auth, persistence);
-                console.log("Authentication Persistence Enabled");
-            } catch (error) {
-                console.log("Error setting persistence: ", error);
-            }
-        };
+  useEffect(() => {
+    const initializeFirebaseAuth = async () => {
+      try {
+        await setPersistence(auth, browserLocalPersistence);
+        console.log("Authentication Persistence Enabled");
+      } catch (error) {
+        console.log("Error setting persistence: ", error);
+      }
+    };
 
-        initializeFirebaseAuth();
-    }, []); // This effect runs only once after component mounts
-
-    const signIn = async () => {
-        setLoading(true);
-        try {
-            const response = await signInWithEmailAndPassword(auth, email, password);
-            console.log(response);
-            // Navigate to the homepage after successful sign-in
-            navigation.navigate('Homescreen');  
-        } catch (error) {
-            console.log(error);
-            alert('Sign In failed: ' + error.message);
-        } finally {
-            setLoading(false);
+    const checkLoginStatus = async () => {
+      try {
+        const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+        if (isLoggedIn === 'true') {
+          // User is logged in, navigate to homepage
+          navigation.navigate('Homescreen');
         }
-    }
-      
-    const signUp = async () => {
-        setLoading(true);
-        try {
-            const response = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(response);
-            alert('Check your email');
-        } catch (error) {
-            console.log(error);
-            alert('Sign In failed: ' + error.message);
-        } finally {
-            setLoading(false);
-        }
-    }
+      } catch (error) {
+        console.log('Error retrieving login status:', error);
+      }
+    };
 
-    return (
-        <View style={styles.container}>
-            <KeyboardAvoidingView behavior="padding">
-                <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize='none' onChangeText={(text) => setEmail(text)} />
-                <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="Password" autoCapitalize='none' onChangeText={(text) => setPassword(text)} />
+    checkLoginStatus(); // Call the check function here
+    initializeFirebaseAuth();
+  }, []); // This effect runs only once after component mounts
 
-                {loading ? <ActivityIndicator size="large" color="#0000ff" />
-                    : (
-                        <View style={styles.buttonContainer}>
-                            <TouchableOpacity style={styles.loginButton} onPress={signIn}>
-                                <Text style={styles.buttonText}>Login</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.createAccountButton} onPress={signUp}>
-                                <Text style={styles.buttonText}>Create Account</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-            </KeyboardAvoidingView>
-        </View>
-    );
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      // Navigate to the homepage after successful sign-in
+      navigation.navigate('Homescreen');
+
+      // Store login status in AsyncStorage
+      await AsyncStorage.setItem('isLoggedIn', 'true');
+    } catch (error) {
+      console.log(error);
+      alert('Sign In failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert('Check your email');
+    } catch (error) {
+      console.log(error);
+      alert('Sign Up failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize='none' onChangeText={(text) => setEmail(text)} />
+        <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="Password" autoCapitalize='none' onChangeText={(text) => setPassword(text)} />
+
+        {loading ? <ActivityIndicator size="large" color="#0000ff" />
+          : (
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.loginButton} onPress={signIn}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.createAccountButton} onPress={signUp}>
+                <Text style={styles.buttonText}>Create Account</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+      </KeyboardAvoidingView>
+    </View>
+  );
 };
 
 export default Login;
