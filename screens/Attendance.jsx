350b832dea06cb07bc92c48ca68e11f8a841e86b
kavindunaveen <it21226878@my.sliit.lk>
2025-02-17 13:31:53 +0530

    attendance link updated

--- screens/Attendance.jsx
+++ screens/Attendance.jsx
@@ -9,16 +9,16 @@ const Attendance = () => {
   return (
     <View style={styles.container}>
       <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 20}}>Select your Company:</Text>
-      <TouchableOpacity style={styles.button1} onPress={() => handlePress('https://script.google.com/macros/s/AKfycby70b4ATK9pkoZ_GG2PVkBxkmxtoelWB5qXr16R1GWhzw9n-BSjS3jrwF3QoSbsu-Yv/exec')}>
+      <TouchableOpacity style={styles.button1} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbyaNiZ4KhVm3l9EEFjxMs0751rdTLsuk9OJHT68M3AePSYf46PIOSne-4tV45eCVZ_cWQ/exec')}>
         <Text style={styles.buttonText}>Everbolt Engineering</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button2} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbydfXPy2l9KJLkNyuwz-I1i39Nm7Gn00cBhRwuRtnfiHHjDLPaIoSJOW7rNDrYYN-32ew/exec')}>
+      <TouchableOpacity style={styles.button2} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbx8VG_ddhemhMgGwB6l4gTbzuzL3FPn8l7YllSYJrXZjUE2gcpsyJ-DuJnkSj1feyIsKg/exec')}>
         <Text style={styles.buttonText}>Everbolt Services</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button3} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbxFYlTaWGHUWSaD-2RB22SXKEJqFbISLNu6L5VjYilqp2WlaoREcavLXYOZZRdZlHTf/exec')}>
+      <TouchableOpacity style={styles.button3} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbzNrG4sI4xZRT0LXBwBQdBw2We6ZgI_DBpBi1SoGhXSQYvKigmZtFUesoJNkutXhwU/exec')}>
         <Text style={styles.buttonText}>Everbolt Foods</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button4} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbz__iDxsNvQ2Yo0481rCRzp6yyizw0KEfRegFVx9i53PbvjLdUrBQZoOvB7rNFrzRWN/exec')}>
+      <TouchableOpacity style={styles.button4} onPress={() => handlePress('https://script.google.com/macros/s/AKfycbxIxVOMeRGf5m8BT7ADgqErlbnHZgM18lnXDh_6OS_t1-AjEDsQJyHSnmOyxODSKNk/exec')}>
         <Text style={styles.buttonText}>CDI Electricals</Text>
       </TouchableOpacity>
     </View>
